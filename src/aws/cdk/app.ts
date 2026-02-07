e2efbc33d8e7407535dc0b50d47a6e349dbf8771
Andreas Lemberger <andi.lemberger@gmx.de>
2026-02-07 20:18:37 +0100

    chore: add custom domain

--- src/aws/cdk/app.ts
+++ src/aws/cdk/app.ts
@@ -1,12 +1,17 @@
 import { App } from "aws-cdk-lib";
 import { TechleStack } from "./techle-stack.js";
 
+process.loadEnvFile();
+
 const app = new App();
 
 new TechleStack(app, "TechleStack", {
   env: {
     region: process.env.CDK_DEFAULT_REGION ?? "eu-central-1",
   },
+  hostedZoneId: process.env.HOSTED_ZONE_ID!,
+  domainName: process.env.DOMAIN_NAME!,
+  apiSubdomain: process.env.API_SUBDOMAIN!,
 });
 
 app.synth();
--- src/aws/cdk/techle-stack.ts
+++ src/aws/cdk/techle-stack.ts
@@ -2,16 +2,42 @@ import { Stack, StackProps, Duration, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
-import { HttpApi, HttpMethod, CorsHttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpApi, HttpMethod, CorsHttpMethod, DomainName } from "aws-cdk-lib/aws-apigatewayv2";
 import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { Certificate, CertificateValidation } from "aws-cdk-lib/aws-certificatemanager";
+import { HostedZone, ARecord, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { ApiGatewayv2DomainProperties } from "aws-cdk-lib/aws-route53-targets";
 import * as path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+interface TechleStackProps extends StackProps {
+  hostedZoneId: string;
+  domainName: string;
+  apiSubdomain: string;
+}
+
 export class TechleStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: TechleStackProps) {
     super(scope, id, props);
+
+    const fullDomain = `${props.apiSubdomain}.${props.domainName}`;
+
+    const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostedZone", {
+      hostedZoneId: props.hostedZoneId,
+      zoneName: props.domainName,
+    });
+
+    const certificate = new Certificate(this, "Certificate", {
+      domainName: fullDomain,
+      validation: CertificateValidation.fromDns(hostedZone),
+    });
+
+    const customDomain = new DomainName(this, "DomainName", {
+      domainName: fullDomain,
+      certificate,
+    });
 
     const dailyWordFn = new NodejsFunction(this, "DailyWordFunction", {
       entry: path.join(__dirname, "..", "lambda", "get-daily-word.ts"),
@@ -38,6 +64,9 @@ export class TechleStack extends Stack {
         allowHeaders: ["Content-Type"],
         maxAge: Duration.hours(24),
       },
+      defaultDomainMapping: {
+        domainName: customDomain,
+      },
     });
 
     httpApi.addRoutes({
@@ -46,8 +75,19 @@ export class TechleStack extends Stack {
       integration: new HttpLambdaIntegration("DailyWordIntegration", dailyWordFn),
     });
 
+    new ARecord(this, "ApiAliasRecord", {
+      zone: hostedZone,
+      recordName: props.apiSubdomain,
+      target: RecordTarget.fromAlias(
+        new ApiGatewayv2DomainProperties(
+          customDomain.regionalDomainName,
+          customDomain.regionalHostedZoneId,
+        ),
+      ),
+    });
+
     new CfnOutput(this, "ApiUrl", {
-      value: httpApi.url ?? "undefined",
+      value: `https://${fullDomain}`,
       description: "URL of the Techle API",
     });
   }
